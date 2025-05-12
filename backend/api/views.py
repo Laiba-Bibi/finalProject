cbc827b796b0260b0ce3e4b763313e2388b6d27f
NoorFatima50 <noorfatima5237@gmail.com>
2025-05-12 20:39:19 +0500

    to run django server
    now this appears on test api (welcome django ) but it completes  when connectivity is done

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,9 +1,7 @@
-from django.shortcuts import render
-
-# from rest_framework.decorators import api_view
+from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from django.http import JsonResponse
 
 @api_view(['GET'])
 def test_api(request):
     return Response({"message": "Hello from Django!"})
-Create your views here.
